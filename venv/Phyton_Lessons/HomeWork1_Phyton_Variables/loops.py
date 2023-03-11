db2b9b5e6e818051726c57d6681e8a38e5a39130
pc <rfkturk43@gmail.com>
2023-03-11 18:15:16 +0300

    döngüler

---
+++ venv/Phyton_Lessons/HomeWork1_Phyton_Variables/loops.py
@@ -0,0 +1,18 @@
+krediler = ["Hızlı Kredi","Maaşını Halkbank'tan Alanlara Özel","Mutlu Emekli İhtiyaç Kredisi"]
+
+#eğer bir dizide kaç eleman olduğunu bilmediğimiz zamanlarda tercih etmemiz gereken döngü tipi
+for kredi in krediler:
+    print(kredi)
+
+for i in range(len(krediler)):
+    print(krediler[i])
+#belirli sayıda dönmesini istedğimiz döngüler için kullandığımız döngü tipi
+for i in range(3,10):
+    print(i)
+
+#belirli bir sayıda farklı artırma veya azaltma değerleri için kullandığımız döngü tipi
+for i in range(0,10,2):
+    print(i)
+
+for kredi in krediler:
+    print("<option>" + kredi+ "</option>")
