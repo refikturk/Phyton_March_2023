c20f5f6a0cca8b3cd7c0826ef44b2f12650550a7
pc <rfkturk43@gmail.com>
2023-03-11 13:40:19 +0300

    First commit for Phyton project_1

---
+++ venv/Phyton_Lessons/HomeWork1_Phyton_Variables/variables.py
@@ -0,0 +1,38 @@
+#Phyton programa diline giriş.
+#Phyton veri tipleri nelerdir?
+#Phyton veri tipleri aşağıda belirtilmiştir.
+# -> String - birbiri ardına gelen karakterlerin oluşturduğu veri tipleridir.
+# -> Rakamsal veri tipleri - integer ve float'tır. integer tam sayıyı , float ise küsürlü rakamları ifade eder.
+# -> bool - atandığı değerin sonucunda true veya false olarak dönüş sağlayan mantıksal sorgulama yapan veri tipidir
+# -> Dizi tipleri - list, tuple, range
+
+# String nedir? String veri/değişken tipleri sıralı karakterlerden oluşan değişken tipleridir Stringler çift tırnak veya
+# tek tırnak içerisinde atanabilir aşağıda birkaç String değişken tipi örneği verilmiştir
+
+str = "Hello World"
+print(str)
+str1 = 'Hello World'
+print(str1)
+print(type(str))
+print(type(str1))
+
+#Rakamsal veri tipleri, tam sayı veya desimal rakamları atayabildiğimiz değişkenlerdir. Phyton programlama dili hangi
+#veri atadığımızı otomatik olarak algıladığı için veri tipini belirtmek gerekmez.
+#int değerleri
+num = 10
+print(type(num))
+
+num2 = 10.0
+print(type(num2))
+
+
+
+
+
+
+
+
+
+
+
+
